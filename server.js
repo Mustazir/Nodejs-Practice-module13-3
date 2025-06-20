2c031864f4db9b7502211b3a90cadd66724bad80
mustazir <billah15-5193@diu.edu.bd>
2025-06-20 18:02:33 +0600

    get single todo

--- server.js
+++ server.js
@@ -1,13 +1,65 @@
 const http = require('http');
 
+const path = require('path');
+
+const fs = require('fs');
+const filepath = path.join(__dirname, './db/todo.json')
+
 const server = http.createServer((req, res) => {
-    if(req.url ==="/todos" && req.method === "GET"){
-        
-    res.end('All Todos');
+
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const pathname = url.pathname;
+
+
+    // get all todos
+    if (pathname === "/todos" && req.method === "GET") {
+        const data = fs.readFileSync(filepath, { encoding: "utf-8" })
+        res.writeHead(200, {
+            "content-type": "application/json"
+        })
+
+        res.end(data);
     }
-    else if(req.url ==="/todos/create-todos" && req.method === "POST"){
+    // Post todo
+    else if (pathname === "/todos/create-todos" && req.method === "POST") {
+        let data = "";
+        req.on("data", (chunk) => {
+            data = data + chunk;
+        })
+        req.on("end", () => {
+            const { title, body } = JSON.parse(data);
+
+            const createdAt = new Date().toLocaleString();
+            const allTodos = fs.readFileSync(filepath, { encoding: "utf-8" })
+            const parsedTodos = JSON.parse(allTodos);
+            parsedTodos.push({ title, body, createdAt });
+            fs.writeFileSync(filepath, JSON.stringify(parsedTodos), { encoding: "utf-8" })
+            res.end(JSON.stringify({ title, body, createdAt }));
+        })
         res.end('Create Todos');
-    }else(
+    }
+    else if (pathname === '/todo' && req.method === "GET") {
+
+        const title =url.searchParams.get('title');
+        console.log(title, 'title')
+
+
+        const data = fs.readFileSync(filepath, { encoding: "utf-8" })
+        const parsedTodos = JSON.parse(data);
+
+        const todo = parsedTodos.find((todo) => todo.title === title);
+        if (!todo) {
+            res.writeHead(404, {
+                "content-type": "application/json"
+            })
+            return res.end(JSON.stringify({ message: "Todo not found" }));
+        }
+
+        
+        res.end('single todo');
+    }
+
+    else (
         res.end('Not Found')
     )
 
