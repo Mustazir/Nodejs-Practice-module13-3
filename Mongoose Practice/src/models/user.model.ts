5d2d1e7032ee026080cbefc820962ce884a7d219
mustazir <billah15-5193@diu.edu.bd>
2025-07-03 18:33:51 +0600

    Custom Validation and Custom error 18.2

--- Mongoose Practice/src/models/user.model.ts
+++ Mongoose Practice/src/models/user.model.ts
@@ -1,54 +1,65 @@
 import { model, Schema } from "mongoose";
 import { IUser } from "../interfaces/user.interfaces";
+import validator from "validator";
 
-const userSchema = new Schema<IUser>({
+const userSchema = new Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: [4, "First name must be at least 4 characters long {VALUE}"],
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "Age must be at least 18 years {VALUE}"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
 
-    firstName:{
-        type: String,
-        required: true,
-        trim: true,
-        minlength: [4,'First name must be at least 4 characters long {VALUE}'],
-    },
-    lastName:{
-        type: String,
-        required: true,
-        trim: true 
-    },
-    age:{
-        type:Number,
-        required: true,
-        min: [18, 'Age must be at least 18 years {VALUE}'],
-    },
-    password:{
-        type: String,
-        required: true,
-   
-    },
-    email:{
-        type: String,
-        validate:{
+    //   Custom validation for email using Libreary
+
+    validate:[validator.isEmail,`{VALUE} is not valid email` ],
+
+      // Custom validation for email
+
+      /*  validate:{
             validator: function(value){
                 return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value); //this will check if the email is valid or not
             },
             message: '{VALUE} is not a valid email address', //this will show the error message if email is not valid
         },
-        required: true,
-        unique: true, //this will make sure that email is unique
-        trim: true ,
-        lowercase: true, //this will make sure that email is always in lowercase
+    */
+
+      required: true,
+      unique: true, //this will make sure that email is unique
+      trim: true,
+      lowercase: true, //this will make sure that email is always in lowercase
     },
-    role:{
-        type: String,
-        enum: {
-            values:['user', 'admin'], //this will make sure that role is either user or admin
-            message: '{VALUE} is not a valid role, must be either user or admin', //this will show the error message if role is not user or admin
-        },
-        default: 'user', //default role is user,
-        lowercase: true, //this will make sure that role is always in lowercase
-    }
-},{
+    role: {
+      type: String,
+      enum: {
+        values: ["user", "admin"], //this will make sure that role is either user or admin
+        message: "{VALUE} is not a valid role, must be either user or admin", //this will show the error message if role is not user or admin
+      },
+      default: "user", //default role is user,
+      lowercase: true, //this will make sure that role is always in lowercase
+    },
+  },
+  {
     versionKey: false, //this will remove __v field from the schema
     timestamps: true, //this will add createdAt and updatedAt fields to the schema
-})
+  }
+);
 
-export const User =model<IUser>("User", userSchema); //here User is the name of the model and userSchema is the schema we created above. This will create a collection called users in the database.
+export const User = model<IUser>("User", userSchema); //here User is the name of the model and userSchema is the schema we created above. This will create a collection called users in the database.
