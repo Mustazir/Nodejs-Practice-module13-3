1b85c2ea377ab570c0ba275133bb8614838f7ab8
mustazir <billah15-5193@diu.edu.bd>
2025-07-11 21:07:04 +0600

    redux first

---
+++ Redux/my-app/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1>Redux Increment Decrement</h1>
+      <button>Increment</button>
+      <div>0</div>
+      <button>Decrement</button>
+    </>
+  );
+}
+
+export default App;
