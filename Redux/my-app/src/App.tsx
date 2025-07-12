d9ce87f0b36180c12b6fe8c110f3b0f2b149709e
mustazir <billah15-5193@diu.edu.bd>
2025-07-12 16:52:01 +0600

    handle incre,decrement intregrater static

--- Redux/my-app/src/App.tsx
+++ Redux/my-app/src/App.tsx
@@ -1,10 +1,23 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/counter/counterSlice";
+
 function App() {
+
+  const dispatch =useDispatch();
+
+  const handleIncrement = ()=>{
+    dispatch(increment())
+  }
+
+  const handleDeccrement = ()=>{
+    dispatch(decrement())
+  }
   return (
     <>
       <h1>Redux Increment Decrement</h1>
-      <button>Increment</button>
+      <button onClick={handleIncrement}>Increment</button>
       <div>0</div>
-      <button>Decrement</button>
+      <button onClick={handleDeccrement}>Decrement</button>
     </>
   );
 }
