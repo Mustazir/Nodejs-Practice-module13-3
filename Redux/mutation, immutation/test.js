5eabaa3e664149f23f3178750318a28e32959afb
mustazir <billah15-5193@diu.edu.bd>
2025-07-13 22:16:39 +0600

    Immutation

---
+++ Redux/mutation, immutation/test.js
@@ -0,0 +1,14 @@
+// its for mutation
+
+const employee ={
+name :"AbortController",
+address:{
+    country:"Bangladsh",city:"dhaka"
+}
+}
+const employee2=employee
+employee2.name="employee"   
+
+console.log(employee)
+console.log(employee2)
+
