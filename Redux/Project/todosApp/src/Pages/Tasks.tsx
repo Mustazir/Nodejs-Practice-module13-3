4d71d6272c54bc393b727442c1208b460e78fb84
mustazir <billah15-5193@diu.edu.bd>
2025-07-18 19:45:39 +0600

    Form

--- Redux/Project/todosApp/src/Pages/Tasks.tsx
+++ Redux/Project/todosApp/src/Pages/Tasks.tsx
@@ -1,23 +1,22 @@
+import { AddTask } from "@/components/module/tasks/AddTask";
 import TaskCard from "@/components/module/tasks/TaskCard";
-import { useAppSelector } from "@/redux/hook"
-import { selectTask } from "@/redux/tasks/taskSlice"
+import { useAppSelector } from "@/redux/hook";
+import { selectTask } from "@/redux/tasks/taskSlice";
 
-export default function  Tasks () {
-  const tasks =useAppSelector(selectTask)
-  console.log(tasks)
-    return (
-
-    
+export default function Tasks() {
+  const tasks = useAppSelector(selectTask);
+  console.log(tasks);
+  return (
     <div className="mx-auto max-w-7xl mt-20">
-      <h1 className="text-white">  This is tasks</h1>
-      <div className="space-y-5 mt-5 ">
-        {tasks.map((task)=>(
-          <TaskCard task={task}></TaskCard>
-        ))} 
-
+      <div className="flex justify-between items-center">
+        <h1 className="text-white"> This is tasks</h1>
+        <AddTask></AddTask>
       </div>
-      
+      <div className="space-y-5 mt-5 ">
+        {tasks.map((task) => (
+          <TaskCard task={task}></TaskCard>
+        ))}
+      </div>
     </div>
   );
-};
-
+}
